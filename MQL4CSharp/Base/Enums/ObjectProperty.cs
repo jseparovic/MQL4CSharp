6220e8af55b04a3a85fee593cf103918ab338b64
Jason Separovic <jseppa01@gmail.com>
2016-01-20 02:23:18 -0800

    added more object functions

---
+++ MQL4CSharp/Base/Enums/ObjectProperty.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQL4CSharp.Base.Enums
+{
+    public enum ObjectProperty
+    {
+        OBJPROP_TIME1	=	0,
+        OBJPROP_PRICE1	=	1,
+        OBJPROP_TIME2	=	2,
+        OBJPROP_PRICE2	=	3,
+        OBJPROP_TIME3	=	4,
+        OBJPROP_PRICE3	=	5,
+        OBJPROP_COLOR	=	6,
+        OBJPROP_STYLE	=	7,
+        OBJPROP_WIDTH	=	8,
+        OBJPROP_BACK	=	9,
+        OBJPROP_RAY	=	10,
+        OBJPROP_ELLIPSE	=	11,
+        OBJPROP_SCALE	=	12,
+        OBJPROP_ANGLE	=	13,
+        OBJPROP_ARROWCODE	=	14,
+        OBJPROP_TIMEFRAMES	=	15,
+        OBJPROP_DEVIATION	=	16,
+        OBJPROP_FONTSIZE	=	100,
+        OBJPROP_CORNER	=	101,
+        OBJPROP_XDISTANCE	=	102,
+        OBJPROP_YDISTANCE	=	103,
+        OBJPROP_FIBOLEVELS	=	200,
+        OBJPROP_LEVELCOLOR	=	201,
+        OBJPROP_LEVELSTYLE	=	202,
+        OBJPROP_LEVELWIDTH	=	203,
+        //OBJPROP_FIRSTLEVEL+n	=	210+n TODO: update firstlevel
+
+    }
+}
