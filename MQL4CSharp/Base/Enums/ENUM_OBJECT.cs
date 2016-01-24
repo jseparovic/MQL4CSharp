c361724ec8b6b6a6cb2631e82b9ceb3de92de0da
jseparovic <jseppa01@gmail.com>
2016-01-23 20:49:46 -0800

    generated mql commands compiling, untested

---
+++ MQL4CSharp/Base/Enums/ENUM_OBJECT.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MQL4CSharp.Base.Enums
+
+{
+    public enum ENUM_OBJECT
+    {
+        OBJ_VLINE = 1,
+        OBJ_HLINE = 2,
+        OBJ_TREND = 3,
+        OBJ_TRENDBYANGLE = 4,
+        OBJ_CYCLES = 5,
+        OBJ_CHANNEL = 6,
+        OBJ_STDDEVCHANNEL = 7,
+        OBJ_REGRESSION = 8,
+        OBJ_PITCHFORK = 9,
+        OBJ_GANNLINE = 10,
+        OBJ_GANNFAN = 11,
+        OBJ_GANNGRID = 12,
+        OBJ_FIBO = 13,
+        OBJ_FIBOTIMES = 14,
+        OBJ_FIBOFAN = 15,
+        OBJ_FIBOARC = 16,
+        OBJ_FIBOCHANNEL = 17,
+        OBJ_EXPANSION = 18,
+        OBJ_RECTANGLE = 19,
+        OBJ_TRIANGLE = 20,
+        OBJ_ELLIPSE = 21,
+        OBJ_ARROW_THUMB_UP = 22,
+        OBJ_ARROW_THUMB_DOWN = 23,
+        OBJ_ARROW_UP = 24,
+        OBJ_ARROW_DOWN = 25,
+        OBJ_ARROW_STOP = 26,
+        OBJ_ARROW_CHECK = 27,
+        OBJ_ARROW_LEFT_PRICE = 28,
+        OBJ_ARROW_RIGHT_PRICE = 29,
+        OBJ_ARROW_BUY = 30,
+        OBJ_ARROW_SELL = 31,
+        OBJ_ARROW = 32,
+        OBJ_TEXT = 33,
+        OBJ_LABEL = 34,
+        OBJ_BUTTON = 35,
+        OBJ_BITMAP = 36,
+        OBJ_BITMAP_LABEL = 37,
+        OBJ_EDIT = 38,
+        OBJ_EVENT = 39,
+        OBJ_RECTANGLE_LABEL = 39
+    }
+}
