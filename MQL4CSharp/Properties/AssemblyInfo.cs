426e6ffb78dfcef2594e0d5fa36d8af44c3f8195
Jason Separovic <jseppa01@gmail.com>
2016-01-30 01:08:51 -0800

    added license

--- MQL4CSharp/Properties/AssemblyInfo.cs
+++ MQL4CSharp/Properties/AssemblyInfo.cs
@@ -5,13 +5,14 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MQL4CSharp")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Metatrader Library for C#")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jason Separovic")]
 [assembly: AssemblyProduct("MQL4CSharp")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2016 Jason Separovic")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -31,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.*")]
